92ed2e875c8760e35dcfac0c868ba6f44c596c2d
drop189 <71636973+drop189@users.noreply.github.com>
2024-05-12 22:29:18 +0300

    Обновление пока не очень, но эта ветка тянет на merge

--- app/src/main/java/hehe/obebos/pizza7000/MainActivity.kt
+++ app/src/main/java/hehe/obebos/pizza7000/MainActivity.kt
@@ -9,12 +9,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 import com.yandex.mapkit.MapKitFactory
 import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     companion object {
         const val MAPKIT_API_KEY = "aa08877f-d508-425c-9274-ed039a4ed158" // API-ключ
@@ -26,11 +29,13 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     private fun setApiKey(savedInstanceState: Bundle?) {
-        val haveApiKey = savedInstanceState?.getBoolean("haveApiKey") ?: false // При первом запуске приложения всегда false
+        val haveApiKey = savedInstanceState?.getBoolean("haveApiKey")
+            ?: false // При первом запуске приложения всегда false
         if (!haveApiKey) {
             MapKitFactory.setApiKey(MAPKIT_API_KEY) // API-ключ должен быть задан единожды перед инициализацией MapKitFactory
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -40,22 +45,64 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setContentView(R.layout.activity_main)
 
         drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)  // Активация toolbar
         setSupportActionBar(toolbar)
 
-        val navigationView = findViewById<NavigationView>(R.id.nav_view) // Активация NavigationDrawer
+        val navigationView =
+            findViewById<NavigationView>(R.id.nav_view) // Активация NavigationDrawer
         navigationView.setNavigationItemSelectedListener(this)
 
-        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav) // Для слепых)
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.open_nav,
+            R.string.close_nav
+        ) // Для слепых)
 
         drawerLayout.addDrawerListener(toggle) // Прослушка пролистываний
         toggle.syncState()
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, HomeFragment()).commit()  // При любых проблемах отсылает на HomeFragment()
+                .replace(R.id.fragment_container, HomeFragment())
+                .commit()  // При любых проблемах отсылает на HomeFragment()
             navigationView.setCheckedItem(R.id.nav_home)
+        }
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.bottom_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+
+                R.id.bottom_settings -> {
+                    replaceFragment(SettingsFragment())
+                    true
+                }
+
+                R.id.bottom_share -> {
+                    replaceFragment(ShareFragment())
+                    true
+                }
+
+                R.id.bottom_about -> {
+                    replaceFragment(AboutFragment())
+                    true
+                }
+
+                R.id.bottom_map -> {
+                    replaceFragment(MapFragment())
+                    true
+                }
+
+                else -> {false}
+            }
         }
     }
 
@@ -63,14 +110,19 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         when (item.itemId) {
             R.id.nav_home -> supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, HomeFragment()).commit()
+
             R.id.nav_map -> supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, MapFragment()).commit()
+
             R.id.nav_settings -> supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, SettingsFragment()).commit()
+
             R.id.nav_share -> supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, ShareFragment()).commit()
+
             R.id.nav_about -> supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, AboutFragment()).commit()
+
             R.id.nav_logout -> {
                 Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show()  // Выход (Крутой)
                 finishAndRemoveTask()
@@ -79,6 +131,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
         drawerLayout.closeDrawer(GravityCompat.START) // Закрывает NavigationDrawer после выполнения функции (Прослушки нажатий)
         return true
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment)
+            .commit()
     }
 
     @Deprecated("Deprecated in Java")
